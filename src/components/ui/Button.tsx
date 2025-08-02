440b5f31f6e31a0fa7806fe4220d51cfcce94dc0
Jack Senyitko <jsenyitko@expensify.com>
2025-08-02 16:57:32 -0500

    tab layout

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,6 +6,7 @@ export default function Button({ className, ...props }: ButtonProps) {
   const buttonClasses = classNames(
     "transition-all",
     "bg-primary-500 text-white",
+    "flex items-center gap-1",
     "text-xs px-3 py-1.5 rounded-lg",
     "hover:bg-primary-600 hover:cursor-pointer",
     "active:bg-primary-700",
--- src/pages/intercept/index.tsx
+++ src/pages/intercept/index.tsx
@@ -1,29 +1,63 @@
+import { useState } from "react";
+import classNames from "classnames";
+import { BracketsCurlyIcon, GearIcon } from "@phosphor-icons/react";
+
 import useRouter from "@/store/router";
 import Button from "@/components/ui/Button";
-import { BracketsCurlyIcon } from "@phosphor-icons/react";
+
+enum Tab {
+  Intercept = "Intercept",
+  History = "History",
+}
 
 export default function Intercept() {
   const router = useRouter();
+  const [tab, setTab] = useState(Tab.Intercept);
+
+  const Tabs = [
+    {
+      label: "Intercept",
+      tab: Tab.Intercept,
+    },
+    {
+      label: "History",
+      tab: Tab.History,
+    },
+  ];
 
   return (
     <>
-      <div className="flex items-center justify-between">
-        <h1>Intercept</h1>
-        <Button onClick={() => router.push("/intercept/configure")}>
-          Configure
-        </Button>
-      </div>
+      <div className="border-b border-gray-200 pb-1 flex items-center text-sm gap-1">
+        {Tabs.map((item) => {
+          const isActive = item.tab === tab;
+          const onClick = () => setTab(item.tab);
 
-      <div className="border-b border-gray-200 py-2 flex items-center text-sm">
-        <div className="px-4 text-gray-500">Intercept</div>
-        <div className="px-4 text-gray-500">History</div>
+          const classes = classNames(
+            "px-4 rounded-md py-1 cursor-pointer",
+            "hover:bg-primary-50 hover:text-primary-500",
+            "active:bg-primary-100 active:text-primary-600",
+            isActive ? "bg-primary-50 text-primary-500" : "text-gray-500"
+          );
+
+          return (
+            <button key={item.tab} className={classes} onClick={onClick}>
+              <p>{item.label}</p>
+            </button>
+          );
+        })}
+
+        <div className="flex-1 flex justify-end">
+          <Button onClick={() => router.push("/intercept/configure")}>
+            <GearIcon size={16} /> Configure
+          </Button>
+        </div>
       </div>
 
       <div className="grid grid-cols-3">
         {new Array(50).fill(0).map((_, i) => (
           <div
             key={i}
-            className="col-span-full odd:bg-primary-50 even:bg-white flex items-center gap-4 px-4 hover:bg-primary-100 text-sm py-1 cursor-default"
+            className="col-span-full odd:bg-primary-50/50 even:bg-white flex items-center gap-4 px-4 hover:bg-primary-100 text-sm py-1 cursor-default"
           >
             <BracketsCurlyIcon size={14} className="text-primary-500" />
             <p>/api/v1/url</p>
