065a27a891583b023a7da056123aeb2ae22e8652
Jack Senyitko <jsenyitko@expensify.com>
2025-08-24 07:15:55 -0400

    add theming to settings

--- ui/pages/settings.tsx
+++ ui/pages/settings.tsx
@@ -1,5 +1,72 @@
-interface SettingsProps {}
+import classNames from "classnames";
+import colors from "tailwindcss/colors";
 
-export default function Settings({}: SettingsProps) {
-  return <></>;
+import usePreferencesStore from "@ui/store/preferences";
+
+export default function Settings() {
+  const theme = usePreferencesStore((s) => s.theme);
+  const setPreference = usePreferencesStore((s) => s.setPreference);
+
+  const themes = [
+    {
+      color: colors.indigo[400],
+      selected: theme === "indigo",
+      onClick: () => setPreference("theme", "indigo"),
+    },
+    {
+      color: colors.red[400],
+      selected: theme === "red",
+      onClick: () => setPreference("theme", "red"),
+    },
+    {
+      color: colors.blue[400],
+      selected: theme === "blue",
+      onClick: () => setPreference("theme", "blue"),
+    },
+    {
+      color: colors.emerald[400],
+      selected: theme === "emerald",
+      onClick: () => setPreference("theme", "emerald"),
+    },
+    {
+      color: colors.fuchsia[400],
+      selected: theme === "fuchsia",
+      onClick: () => setPreference("theme", "fuchsia"),
+    },
+  ];
+
+  return (
+    <>
+      <h1>Settings</h1>
+
+      <div className="flex items-center gap-1">
+        {themes.map((item) => (
+          <ThemeSelector key={item.color} {...item} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+interface ThemeSelectorProps {
+  selected: boolean;
+  color?: string;
+  onClick?: () => void;
+}
+
+function ThemeSelector({ selected, color, onClick }: ThemeSelectorProps) {
+  const classes = classNames("rounded-full cursor-pointer", !selected ? "size-8" : "size-6");
+
+  if (selected) {
+    return (
+      <div
+        style={{ borderColor: color }}
+        className="flex items-center justify-center border-2 size-8 rounded-full"
+      >
+        <div className={classes} style={{ backgroundColor: color }} />
+      </div>
+    );
+  }
+
+  return <button className={classes} style={{ backgroundColor: color }} onClick={onClick} />;
 }
--- ui/pages/shortcuts.tsx
+++ ui/pages/shortcuts.tsx
@@ -1,83 +1,11 @@
 import Button from "@ui/components/ui/Button";
-import { useEffect, useRef, useState } from "react";
-
-const items = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
 
 export default function Shortcuts() {
-  const VIEWPORT_HEIGHT = 600;
-  const ITEM_HEIGHT = 24;
-  const ITEM_COUNT = 10000;
-
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [scrollTop, setScrollTop] = useState(0);
-
-  const overscan = 5;
-  const visibleCount = Math.ceil(VIEWPORT_HEIGHT / ITEM_HEIGHT);
-  const startIndex = Math.max(0, Math.floor(scrollTop / ITEM_HEIGHT) - overscan);
-  const endIndex = Math.min(items.length, startIndex + visibleCount + overscan * 2);
-
-  useEffect(() => {
-    let ticking = false;
-    const container = containerRef.current;
-
-    const handleScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          setScrollTop(container!.scrollTop);
-          ticking = false;
-        });
-        ticking = true;
-      }
-    };
-
-    container?.addEventListener("scroll", handleScroll);
-    return () => container?.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const visibleItems = items.slice(startIndex, endIndex);
-  const totalHeight = ITEM_COUNT * ITEM_HEIGHT;
-  const offsetY = startIndex * ITEM_HEIGHT;
-
   return (
     <>
       <div className="flex items-center justify-between">
         <h1>Shortcuts</h1>
         <Button>Create</Button>
-      </div>
-
-      <div
-        ref={containerRef}
-        className="relative overflow-y-auto"
-        style={{ height: VIEWPORT_HEIGHT }}
-      >
-        <table className="w-full table-fixed relative" style={{ height: totalHeight }}>
-          <colgroup>
-            {/* First column stretches */}
-            <col className="w-full" />
-            {/* Second column fixed at 40px */}
-            <col style={{ width: 70 }} />
-          </colgroup>
-
-          <tbody>
-            {/* Spacer before visible rows */}
-            <tr style={{ height: offsetY }} />
-            {/* Visible rows */}
-            {visibleItems.map((item) => (
-              <tr
-                key={item}
-                style={{ height: ITEM_HEIGHT }}
-                className="even:bg-red-500 odd:bg-red-200"
-              >
-                <td className="w-auto">{item}</td>
-                <td className="min-w-0 overflow-hidden" style={{ width: 70 }}>
-                  {item}
-                </td>
-              </tr>
-            ))}
-            {/* Spacer after visible rows */}
-            <tr style={{ height: totalHeight - offsetY - visibleItems.length * ITEM_HEIGHT }} />
-          </tbody>
-        </table>
       </div>
     </>
   );
