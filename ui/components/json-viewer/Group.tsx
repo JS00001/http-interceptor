032e18c912c93c5505f30b17cc5955b5bfb6b90e
Jack Senyitko <jsenyitko@expensify.com>
2025-08-24 12:12:25 -0400

    fix font sizing

--- ui/components/json-viewer/Group.tsx
+++ ui/components/json-viewer/Group.tsx
@@ -10,8 +10,8 @@ export default function Group({ nodeKey, expanded, level, value }: GroupProps) {
 
   return (
     <div style={{ paddingLeft: level * 16 }} className="w-full hover:bg-primary-50 -ml-4">
-      <p className="truncate text-gray-800 w-full">
-        <span className="mr-2 text-[12px]">{arrow}</span>
+      <p className="truncate text-gray-800 w-full text-[11px]">
+        <span className="mr-2">{arrow}</span>
         <span className="text-fuchsia-800 shrink-0">{nodeKey}</span>
         {nodeKey && <span className="mr-2">:</span>}
         {value}
