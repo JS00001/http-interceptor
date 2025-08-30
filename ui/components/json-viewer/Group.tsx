56195cacd6b1e5c4588b3283ca93c687ed2ed28c
Jack Senyitko <jsenyitko@expensify.com>
2025-08-30 12:53:46 -0400

    try and fix windows sizing

--- ui/components/json-viewer/Group.tsx
+++ ui/components/json-viewer/Group.tsx
@@ -11,9 +11,9 @@ export default function Group({ nodeKey, expanded, level, value }: GroupProps) {
   return (
     <div style={{ paddingLeft: level * 16 }} className="w-full hover:bg-primary-50 -ml-4">
       <p className="truncate text-gray-800 w-full text-[11px]">
-        <span className="mr-2">{arrow}</span>
-        <span className="text-fuchsia-800 shrink-0">{nodeKey}</span>
-        {nodeKey && <span className="mr-2">:</span>}
+        <span className="mr-2 text-[11px]">{arrow}</span>
+        <span className="text-fuchsia-800 shrink-0 text-[11px]">{nodeKey}</span>
+        {nodeKey && <span className="mr-2 text-[11px]">:</span>}
         {value}
       </p>
     </div>
