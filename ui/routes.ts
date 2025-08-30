1c7a714392d17ab4848099f7ff954b731cbcc3b3
Jack Senyitko <jsenyitko@expensify.com>
2025-08-30 12:21:56 -0400

    delete s hortcuts

--- ui/routes.ts
+++ ui/routes.ts
@@ -1,12 +1,10 @@
 import Settings from '@ui/pages/settings';
 import Intercept from '@ui/pages/intercept';
-import Shortcuts from '@ui/pages/shortcuts';
 
 export type Route = keyof typeof Routes;
 
 const Routes = {
   '/intercept': Intercept,
-  '/shortcuts': Shortcuts,
   '/settings': Settings,
 } as const;
 
