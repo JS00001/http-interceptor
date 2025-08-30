5c32e0ef0d6bfd930f88662bbf280c61db0845bb
Jack Senyitko <jsenyitko@expensify.com>
2025-08-30 13:31:21 -0400

    finish allowing the storing of headers

--- ui/components/modals/Configure/index.tsx
+++ ui/components/modals/Configure/index.tsx
@@ -9,6 +9,7 @@ import Modal from "@ui/components/ui/Modal";
 import Button from "@ui/components/ui/Button";
 import TabBar from "@ui/components/ui/TabBar";
 import useRulesStore from "@shared/stores/interceptor-rules";
+import usePreferencesStore from "@shared/stores/preferences";
 
 enum Tab {
   Rules = "Rules",
@@ -32,8 +33,10 @@ const Tabs = [
 export default function ConfigureModal() {
   const [tab, setTab] = useState(Tab.Rules);
   const close = useModalStore((s) => s.close);
-  const addRule = useRulesStore((s) => s.addRule);
   const isOpen = useModalStore((s) => s.modals.configure);
+
+  const addRule = useRulesStore((s) => s.addRule);
+  const addHeader = usePreferencesStore((s) => s.addHeader);
 
   const onClose = () => {
     close("configure");
@@ -47,7 +50,9 @@ export default function ConfigureModal() {
         <h2>Configure Interceptor</h2>
 
         <div className="self-end flex items-center gap-2">
-          <Button onClick={addRule}>Add Rule</Button>
+          {tab === Tab.Rules && <Button onClick={addRule}>Add Rule</Button>}
+          {tab === Tab.CustomHeaders && <Button onClick={addHeader}>Add Header</Button>}
+
           <Button color="secondary" onClick={onClose}>
             Close <XIcon size={14} />
           </Button>
